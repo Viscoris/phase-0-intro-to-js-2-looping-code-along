83e69660bec8f21dad7abd9c666051b5941cd08a
Daniel Waghorne <dwaghorne@hotmail.com>
2024-01-10 12:59:10 +1000

    lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function writeCards(names, surprise) {
+    const thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+      const name = names[i];
+      const message = `Thank you, ${name}, for the wonderful ${surprise} gift!`;
+      thankYouMessages.push(message);
+    }
+    return thankYouMessages;
+  }
+
+
+function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+
+
+
